83f5ccbe8f979534abb484ccb9036de43eba2fde
devanshguptajp <devanshguptajp@gmail.com>
2026-09-24 20:47:17 +0530

    Fix Vanya field KeyboardOptions import

--- app/src/main/java/com/tingtring/talk/ui/VanyaCorrectionField.kt
+++ app/src/main/java/com/tingtring/talk/ui/VanyaCorrectionField.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.input.KeyboardType
 
